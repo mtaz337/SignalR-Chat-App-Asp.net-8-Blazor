b0ffc306e2b621cfca09da12e3a9a7f33eccb060
mtaz337 <mahirtaz@gmail.com>
2023-11-28 14:35:36 +0600

    initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalRApp.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
